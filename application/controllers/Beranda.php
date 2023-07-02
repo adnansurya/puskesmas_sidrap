26a682e47d364fafe388f1422903c83a35629be2
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2023-07-02 15:28:25 +0700

    ubah variabel nama di beranda

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -5,7 +5,7 @@ class Beranda extends CI_Controller {
 	public function index()
 	{
         $data['title'] = "Beranda";
-        $data['nama'] = "Puskesmas Sidrap";
+        $data['nama'] = "Puskesmas Tanrutedong";
 		$this->load->view('beranda/header', $data);
 		$this->load->view('beranda/style', $data);
 		$this->load->view('beranda/index', $data);
